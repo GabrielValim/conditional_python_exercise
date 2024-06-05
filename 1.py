c1795727a59cc4fdc1c4c389a1ed9dd488c2a0e2
GabrielValim <barros.gabriel.valim@hotmail.com>
2024-06-05 20:07:45 -0300

    exercicios de condicional

---
+++ 1.py
@@ -0,0 +1,8 @@
+# 1 - Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar. 
+
+valor = input ("Insira o numero 0");
+
+if valor % 2 == 0:
+    print("Valor é par!")
+else:
+    print("Valor é impar!")
